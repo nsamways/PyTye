26dc29a4f049aa91f8ded4e7e29c0e78ccf54d9d
nsamways <neale.samways@bcu.ac.uk>
2018-08-31 13:09:02 +0100

    Added main script (PyTeye)

---
+++ ptanalyser/pyteye.py
@@ -0,0 +1,17 @@
+'''
+Created on 31 Aug 2018
+
+@author: ID121943
+'''
+
+import os, numpy
+
+if __name__ == '__main__':
+    # run the script
+    
+    
+    # read in files
+    # behavioural
+    
+    print("Running PyTeye")
+    
